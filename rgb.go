f14bddb0ce150e481526d10fa329b963e0b1ba6f
guojia <guojia@guojia.com>
2023-08-11 11:25:44 +0800

    cpr

--- rgb.go
+++ rgb.go
@@ -1,3 +1,9 @@
+/*
+ *  * Copyright (c) 2023 guojia99 All rights reserved.
+ *  * Created: 2023/2/26 下午5:22.
+ *  * Author: guojia(https://github.com/guojia99)
+ */
+
 package go_color
 
 import "math"
